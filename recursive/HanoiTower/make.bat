da59bc4bd2e09c51ae1f89b82a38f352625f3b39
chauttm <chauttm.vnu@gmail.com>
2023-04-02 23:51:30 +0700

    update

--- recursive/HanoiTower/make.bat
+++ recursive/HanoiTower/make.bat
@@ -1,1 +1,8 @@
- g++ -std=c++11 -ID:\setup\SDL2-2.0.5\x86_64-w64-mingw32\include\SDL2 -LD:\setup\SDL2-2.0.5\x86_64-w64-mingw32\lib main.cpp SDL_utils.cpp -o a.exe -lmingw32 -lSDL2main -lSDL2
+set OBJS=RecursiveSquares.cpp SDL_utils.cpp
+set OBJ_NAME=main.exe
+set COMPILER_FLAGS=-std=c++11
+set LINKER_FLAGS=-lmingw32 -lSDL2main -lSDL2
+set INCLUDE_DIR=-IG:\SDL2-2.0.14\x86_64-w64-mingw32\include\SDL2
+set LIB_DIR=-LG:\SDL2-2.0.14\x86_64-w64-mingw32\lib
+
+g++ %COMPILER_FLAGS% %INCLUDE_DIR% %LIB_DIR% %OBJS% %LINKER_FLAGS% -o %OBJ_NAME%
