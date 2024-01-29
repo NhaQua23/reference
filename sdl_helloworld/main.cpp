5136ae828d5cd4423bac638a4ed0c707211d0b2d
chauttm <chauttm.vnu@gmail.com>
2024-01-29 23:11:44 +0700

    better code

--- sdl_helloworld/main.cpp
+++ sdl_helloworld/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-const string WINDOW_TITLE = "Hello World!";
+const char* WINDOW_TITLE = "Hello World!";
 
 void logSDLError(std::ostream& os,
                  const std::string &msg, bool fatal)
@@ -17,25 +17,32 @@ void logSDLError(std::ostream& os,
     }
 }
 
-void initSDL(SDL_Window* &window, SDL_Renderer* &renderer,
-             int SCREEN_WIDTH, int SCREEN_HEIGHT, const string& WINDOW_TITLE)
+SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
         logSDLError(std::cout, "SDL_Init", true);
 
-    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     //full screen
-    //window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
     if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+    return window;
+}
+
+SDL_Renderer* createRenderer(SDL_Window* window)
+{
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
                                               SDL_RENDERER_PRESENTVSYNC);
+    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
     //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
 
     if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
 
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
     SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    return renderer;
 }
 
 void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
@@ -73,9 +80,8 @@ void drawSomething(SDL_Window* window, SDL_Renderer* renderer) {
 int main(int argc, char* argv[])
 {
     //Khởi tạo môi trường đồ họa
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    initSDL(window, renderer, SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SDL_Window* window = initSDL(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SDL_Renderer* renderer = createRenderer(window);
 
     //Xóa màn hình
     SDL_RenderClear(renderer);
@@ -84,9 +90,9 @@ int main(int argc, char* argv[])
     drawSomething(window, renderer);
 
     //Hiện bản vẽ ra màn hình
-    //Khi thông thường chạy với môi trường bình thường ở nhà
+    //Khi chạy tại môi trường bình thường
     SDL_RenderPresent(renderer);
-    //Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
+    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
     //SDL_UpdateWindowSurface(window);
 
     //Đợi phím bất kỳ trước khi đóng môi trường đồ họa và kết thúc chương trình
