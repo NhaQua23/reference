e18266042c661cc2eae8986edeff5ab5f031c567
chauttm <chauttm.vnu@gmail.com>
2024-01-30 13:43:54 +0700

    new logging function

--- sdl_helloworld/main.cpp
+++ sdl_helloworld/main.cpp
@@ -7,25 +7,21 @@ const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const char* WINDOW_TITLE = "Hello World!";
 
-void logSDLError(std::ostream& os,
-                 const std::string &msg, bool fatal)
+void logErrorAndExit(const char* msg, const char* error)
 {
-    os << msg << " Error: " << SDL_GetError() << std::endl;
-    if (fatal) {
-        SDL_Quit();
-        exit(1);
-    }
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
+    SDL_Quit();
 }
 
 SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-        logSDLError(std::cout, "SDL_Init", true);
+        logErrorAndExit("SDL_Init", SDL_GetError());
 
     SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     //full screen
     //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
 
     return window;
 }
@@ -37,7 +33,7 @@ SDL_Renderer* createRenderer(SDL_Window* window)
     //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
     //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
 
-    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
 
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
     SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
--- sdl_image/main.cpp
+++ sdl_image/main.cpp
@@ -8,25 +8,24 @@ const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const char* WINDOW_TITLE = "Hello World!";
 
-void logSDLError(std::ostream& os,
-                 const std::string &msg, bool fatal)
+void logErrorAndExit(const char* msg, const char* error)
 {
-    os << msg << " Error: " << SDL_GetError() << std::endl;
-    if (fatal) {
-        SDL_Quit();
-        exit(1);
-    }
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
+    SDL_Quit();
 }
 
 SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-        logSDLError(std::cout, "SDL_Init", true);
+        logErrorAndExit("SDL_Init", SDL_GetError());
 
     SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     //full screen
     //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
+
+    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
+        logErrorAndExit( "SDL_image error:", IMG_GetError());
 
     return window;
 }
@@ -38,7 +37,7 @@ SDL_Renderer* createRenderer(SDL_Window* window)
     //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
     //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
 
-    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
 
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
     SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -48,6 +47,8 @@ SDL_Renderer* createRenderer(SDL_Window* window)
 
 void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
 {
+    IMG_Quit();
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
@@ -64,21 +65,26 @@ void waitUntilKeyPressed()
     }
 }
 
-SDL_Texture* loadTexture(const char* path, SDL_Renderer* renderer )
+void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer)
 {
-    SDL_Texture* newTexture = nullptr;
-    SDL_Surface* loadedSurface = IMG_Load(path);
-    if ( loadedSurface == nullptr )
-        cout << "Unable to load image " << path << " SDL_image Error: "
-             << IMG_GetError() << endl;
-    else {
-        newTexture = SDL_CreateTextureFromSurface( renderer, loadedSurface );
-        if( newTexture == nullptr )
-            cout << "Unable to create texture from " << path << " SDL Error: "
-                 << SDL_GetError() << endl;
-        SDL_FreeSurface( loadedSurface );
-    }
-    return newTexture;
+	SDL_Rect dest;
+
+	dest.x = x;
+	dest.y = y;
+	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+
+	SDL_RenderCopy(renderer, texture, NULL, &dest);
+}
+
+SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer)
+{
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
+
+	SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
+	if (texture == NULL)
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
+
+	return texture;
 }
 
 int main(int argc, char *argv[])
@@ -93,11 +99,7 @@ int main(int argc, char *argv[])
     waitUntilKeyPressed();
 
     SDL_Texture* spongeBob = loadTexture("Spongebob.png", renderer);
-    SDL_Rect spongeBobRect;
-    SDL_QueryTexture(spongeBob, NULL, NULL, &spongeBobRect.w, &spongeBobRect.h);
-    spongeBobRect.x = 100;
-    spongeBobRect.y = 100;
-    SDL_RenderCopy( renderer, spongeBob, NULL, &spongeBobRect );
+    renderTexture(spongeBob, 200, 200, renderer);
 
     SDL_RenderPresent( renderer );
     waitUntilKeyPressed();
